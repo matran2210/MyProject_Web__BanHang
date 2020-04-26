5bfb9d78c90c7cc9652b272e9224e20c496780e0
matran2210 <64309499+matran2210@users.noreply.github.com>
2020-04-26 10:51:54 +0700

    Update app.php

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => (bool) env('APP_DEBUG', false),
+    'debug' => (bool) env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -119,7 +119,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY','base64:mmJoe3DisHzNmpqUkczZSSaJLR5lCPs+0rfnewek2r8='),
 
     'cipher' => 'AES-256-CBC',
 
